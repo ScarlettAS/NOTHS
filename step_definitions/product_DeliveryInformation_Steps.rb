04a23e4d7975f9ed8bfa78726d7d1615bfc98852
Scarlett Anderson Salewski <scarlett@Scarletts-Air.home>
2018-03-22 09:08:38 +0000

    sas

--- step_definitions/product_DeliveryInformation_Steps.rb
+++ step_definitions/product_DeliveryInformation_Steps.rb
@@ -2,7 +2,7 @@ require 'selenium-webdriver'
 require 'minitest/spec'
 
 browser = Selenium::WebDriver.for :chrome
-page_url = ""
+page_url = ''
 
 Given(/^the product page URL is "(.*?)"$/) do |url|
   page_url = url
@@ -14,16 +14,16 @@ When(/^I navigate to the product page$/) do
 end
 
 Then(/^the delivery summary is shown$/) do
-  delivery_summary = browser.find_element(id: "delivery_summary")
+  delivery_summary = browser.find_element(id: 'delivery_summary')
   assert(delivery_summary.displayed?)
 end
 
 And(/^I click on the delivery tab$/) do
-  delivery_tab_item = browser.find_element(id: "delivery_tab_item")
+  delivery_tab_item = browser.find_element(id: 'delivery_tab_item')
   delivery_tab_item.click
 end
 
 Then (/^I can see the detailed delivery information$/) do
-  delivery_tab = browser.find_element(id: "delivery_tab")
+  delivery_tab = browser.find_element(id: 'delivery_tab')
   assert(delivery_tab.displayed?)
 end
